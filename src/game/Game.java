9ad15924acd73672dd4eb313e263fb0379399c26
Bartek <bartek9393@poczta.onet.pl>
2017-02-25 16:31:03 +0100

    Changed number of default player tokens to 0

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,7 +4,7 @@ public class Game {
     static Tokens tokens = new Tokens(7, 5);
 
     public static void main(String[] args) {
-        Player player = new Player(new Tokens(7, 5));
+        Player player = new Player(new Tokens(0, 0));
         new CharacterCardCost().displayCost();
     }
 }
