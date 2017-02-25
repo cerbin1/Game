5ff1987246d0c13d975d6141cb95accc5902dc77
Bartek <bartek9393@poczta.onet.pl>
2017-02-25 18:09:04 +0100

    Added initializePlayers()

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,9 +2,19 @@ package game;
 
 public class Game {
     static Tokens tokens = new Tokens(7, 5);
+    private Player[] players = new Player[4];
+
+    private void startGame() {
+        initializePlayers();
+    }
+
+    private void initializePlayers() {
+        for (int i = 0; i < players.length; i++) {
+            players[i] = new Player(new Tokens(0, 0));
+        }
+    }
 
     public static void main(String[] args) {
-        Player player = new Player(new Tokens(0, 0));
-        new CharacterCardCost().displayCost();
+        new Game().startGame();
     }
 }
