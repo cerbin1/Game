0629f72f38e36dfa71941398366c65e6ca9c9f38
Bartek <bartek9393@poczta.onet.pl>
2017-02-26 20:50:38 +0100

    Added testing getRandomCheapCardCost()

--- test/test/CardCostTest.java
+++ test/test/CardCostTest.java
@@ -4,23 +4,59 @@ import game.CardCost;
 import game.Tokens;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 public class CardCostTest {
-    private CardCost cardCost = new CardCost(new MockRandom(0, 0));
 
     @Test
-    public void shouldRandomFirstTypeOfCheapCardCost() {
+    public void shouldRandomOneTypeOfCheapCostCard() {
         // given
+        CardCost cardCost = new CardCost(new MockRandom(0, 0));
         Tokens tokens = cardCost.getRandomCheapCardCost();
 
         // when
-        List<Integer> tokensValues = Arrays.asList(tokens.getBlack(), tokens.getBlue(), tokens.getGreen(), tokens.getRed(), tokens.getWhite());
+        int[] tokensValue = {tokens.getGreen(), tokens.getWhite(), tokens.getBlue(), tokens.getBlack(), tokens.getRed()};
 
         // then
-        assertTrue(tokensValues.contains(3));
+        assertArrayEquals(tokensValue, new int[]{3, 0, 0, 0, 0});
+    }
+
+    @Test
+    public void shouldRandomTwoTypesOfCheapCostCard() {
+        // given
+        CardCost cardCost = new CardCost(new MockRandom(2, 2, 3));
+        Tokens tokens = cardCost.getRandomCheapCardCost();
+
+        // when
+        int[] tokensValue = {tokens.getGreen(), tokens.getWhite(), tokens.getBlue(), tokens.getBlack(), tokens.getRed()};
+
+        // then
+        assertArrayEquals(tokensValue, new int[]{0, 0, 2, 2, 0});
+    }
+
+    @Test
+    public void shouldRandomThreeTypesOfCheapCostCard() {
+        // given
+        CardCost cardCost = new CardCost(new MockRandom(4, 0, 1, 4));
+        Tokens tokens = cardCost.getRandomCheapCardCost();
+
+        // when
+        int[] tokensValue = {tokens.getGreen(), tokens.getWhite(), tokens.getBlue(), tokens.getBlack(), tokens.getRed()};
+
+        // then
+        assertArrayEquals(tokensValue, new int[]{3, 1, 0, 0, 1});
+    }
+
+    @Test
+    public void shouldRandomFourTypesOfCheapCostCard() {
+        // given
+        CardCost cardCost = new CardCost(new MockRandom(6, 0, 1, 2, 4));
+        Tokens tokens = cardCost.getRandomCheapCardCost();
+
+        // when
+        int[] tokensValue = {tokens.getGreen(), tokens.getWhite(), tokens.getBlue(), tokens.getBlack(), tokens.getRed()};
+
+        // then
+        assertArrayEquals(tokensValue, new int[]{2, 1, 1, 0, 1});
     }
 }
