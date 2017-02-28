a415296c702910095b479764f5111ac17cbae62a
Bartek <bartek9393@poczta.onet.pl>
2017-02-28 20:01:51 +0100

    Removed points

--- src/game/cards/Card.java
+++ src/game/cards/Card.java
@@ -3,16 +3,11 @@ package game.cards;
 import game.Tokens;
 
 public class Card {
-    private int points;
     private boolean reserved = false;
     private Tokens cost;
 
     public Card(Tokens cost) {
         this.cost = cost;
-    }
-
-    public int getPoints() {
-        return points;
     }
 
     public boolean isReserved() {
