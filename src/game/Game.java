2c27037ce4e16487de6f02f21ee2410619e897cd
Bartek <bartek9393@poczta.onet.pl>
2017-02-28 19:13:07 +0100

    Removed field startingPlayer

--- src/game/Game.java
+++ src/game/Game.java
@@ -11,7 +11,6 @@ public class Game {
     private List<Card> mediumCards;
     private List<Card> expensiveCards;
     private List<Player> players;
-    private Player startingPlayer;
     private int currentPlayer = 0;
 
     Game(Tokens tokens, List<Player> players, List<Card> cheapCards, List<Card> mediumCards, List<Card> expensiveCards) {
@@ -20,13 +19,6 @@ public class Game {
         this.cheapCards = cheapCards;
         this.mediumCards = mediumCards;
         this.expensiveCards = expensiveCards;
-        this.startingPlayer = setStartingPlayer();
-        this.currentPlayer = players.indexOf(startingPlayer);
-    }
-
-    private Player setStartingPlayer() {
-        Player randomStartingPlayer = players.get(new Random().nextInt(players.size()));
-        return randomStartingPlayer;
     }
 
     public Tokens getTokens() {
