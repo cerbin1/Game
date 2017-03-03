ffce17c63a140d9ec087e0333718b392befbd266
Daniel <wilkowski.kontakt@gmail.com>
2017-03-04 00:59:21 +0100

    [View] Separated tokens into separate files

--- src/app/game/view/ImageRepository.java
+++ src/app/game/view/ImageRepository.java
@@ -10,7 +10,13 @@ public class ImageRepository {
     public BufferedImage cardCastle = get("images/cards/view/castle.png");
 
     public BufferedImage background1 = get("images/background_1.png");
-    public BufferedImage tokens = get("images/tokens.png");
+
+    public BufferedImage tokenGreen = get("images/tokens/green.png");
+    public BufferedImage tokenPurple = get("images/tokens/purple.png");
+    public BufferedImage tokenBlue = get("images/tokens/blue.png");
+    public BufferedImage tokenBlack = get("images/tokens/black.png");
+    public BufferedImage tokenRed = get("images/tokens/red.png");
+    public BufferedImage tokenVersatile = get("images/tokens/versatile.png");
 
     private BufferedImage get(String filename) {
         return Load.image(filename);
