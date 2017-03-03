b36218c69c209d3eef44da0da61831b6433fc255
Bartek <bartek9393@poczta.onet.pl>
2017-03-03 20:49:46 +0100

    Added CardColorGeneratorTest

---
+++ test/CardColorGeneratorTest.java
@@ -0,0 +1,29 @@
+import app.game.TokenColor;
+import app.game.card.CardColorGenerator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardColorGeneratorTest {
+
+    @Test
+    public void shouldGenerateCardColor() {
+        // given
+        MockRandom random = new MockRandom(0, 1, 2, 3, 4);
+        CardColorGenerator colorGenerator = new CardColorGenerator(random);
+
+        // when
+        TokenColor cardColor1 = colorGenerator.generateColor();
+        TokenColor cardColor2 = colorGenerator.generateColor();
+        TokenColor cardColor3 = colorGenerator.generateColor();
+        TokenColor cardColor4 = colorGenerator.generateColor();
+        TokenColor cardColor5 = colorGenerator.generateColor();
+
+        // then
+        assertEquals(cardColor1, TokenColor.Green);
+        assertEquals(cardColor2, TokenColor.Purple);
+        assertEquals(cardColor3, TokenColor.Blue);
+        assertEquals(cardColor4, TokenColor.Black);
+        assertEquals(cardColor5, TokenColor.Red);
+    }
+}
