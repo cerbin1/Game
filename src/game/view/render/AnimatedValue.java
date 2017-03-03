7e7027095e31f94904f545667dc0b16de7a22e6a
Daniel <wilkowski.kontakt@gmail.com>
2017-03-03 02:10:21 +0100

    [View] Added AnimatedValue

---
+++ src/game/view/render/AnimatedValue.java
@@ -0,0 +1,39 @@
+package game.view.render;
+
+import static java.lang.Double.compare;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
+public class AnimatedValue {
+    private final double changePerSecond;
+    private double currentValue;
+    private double destinationValue;
+
+    public AnimatedValue(double value) {
+        this(value, 1.0);
+    }
+
+    public AnimatedValue(double value, double changePerSecond) {
+        this.currentValue = value;
+        this.destinationValue = value;
+        this.changePerSecond = changePerSecond;
+    }
+
+    public void setValue(double value) {
+        this.destinationValue = value;
+    }
+
+    public double getValue() {
+        return currentValue;
+    }
+
+    public void update(double seconds) {
+        int compare = compare(currentValue, destinationValue);
+        if (compare < 0) {
+            currentValue = min(currentValue + seconds * changePerSecond, destinationValue);
+        }
+        if (compare > 0) {
+            currentValue = max(currentValue - seconds * changePerSecond, destinationValue);
+        }
+    }
+}
---
+++ test/AnimatedValueTest.java
@@ -0,0 +1,82 @@
+import game.view.render.AnimatedValue;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class AnimatedValueTest {
+    @Test
+    public void shouldGetValue() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(14);
+
+        // when
+        double value = animatedValue.getValue();
+
+        // then
+        assertEquals(value, 14, 0.000001);
+    }
+
+    @Test
+    public void shouldUpdateToCurrent() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(14);
+
+        // when
+        animatedValue.update(0.1);
+
+        // then
+        assertEquals(animatedValue.getValue(), 14, 0.000001);
+    }
+
+    @Test
+    public void shouldUpdateUpTo() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(10);
+        animatedValue.setValue(11);
+
+        // when
+        animatedValue.update(0.1);
+
+        // then
+        assertEquals(animatedValue.getValue(), 10.1, 0.000001);
+    }
+
+    @Test
+    public void shouldUpdateDownTo() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(10);
+        animatedValue.setValue(9);
+
+        // when
+        animatedValue.update(0.1);
+
+        // then
+        assertEquals(animatedValue.getValue(), 9.9, 0.000001);
+    }
+
+    @Test
+    public void shouldUpdateUpToLimit() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(10);
+        animatedValue.setValue(11);
+
+        // when
+        animatedValue.update(1.5);
+
+        // then
+        assertEquals(animatedValue.getValue(), 11.0, 0.000001);
+    }
+
+    @Test
+    public void shouldUpdateDownToLimit() {
+        // given
+        AnimatedValue animatedValue = new AnimatedValue(10);
+        animatedValue.setValue(9);
+
+        // when
+        animatedValue.update(1.5);
+
+        // then
+        assertEquals(animatedValue.getValue(), 9.0, 0.000001);
+    }
+}
