ff95bd8bf420d02e3650fa8f7c843ddb847891ba
Bartek <bartek9393@poczta.onet.pl>
2017-03-03 20:36:28 +0100

    Added CardPointsGeneratorTest

---
+++ test/CardPointsGeneratorTest.java
@@ -0,0 +1,56 @@
+import app.game.card.CardPointsGenerator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardPointsGeneratorTest {
+
+    @Test
+    public void shouldGenerateCheapCardPoints() {
+        // given
+        MockRandom random = new MockRandom(0, 1);
+        CardPointsGenerator generator = new CardPointsGenerator(random);
+
+        // when
+        int cheapCardPoints1 = generator.generateCheapCardPoints();
+        int cheapCardPoints2 = generator.generateCheapCardPoints();
+
+        // then
+        assertEquals(cheapCardPoints1, 0);
+        assertEquals(cheapCardPoints2, 1);
+    }
+
+    @Test
+    public void shouldGenerateMediumCardPoints() {
+        // given
+        MockRandom random = new MockRandom(0, 1, 2);
+        CardPointsGenerator generator = new CardPointsGenerator(random);
+
+        // when
+        int mediumCardPoints1 = generator.generateMediumCardPoints();
+        int mediumCardPoints2 = generator.generateMediumCardPoints();
+        int mediumCardPoints3 = generator.generateMediumCardPoints();
+
+        // then
+        assertEquals(mediumCardPoints1, 1);
+        assertEquals(mediumCardPoints2, 2);
+        assertEquals(mediumCardPoints3, 3);
+    }
+
+    @Test
+    public void shouldGenerateExpensiveCardPoints() {
+        // given
+        MockRandom random = new MockRandom(0, 1, 2);
+        CardPointsGenerator generator = new CardPointsGenerator(random);
+
+        // when
+        int expensiveCardPoints1 = generator.generateExpensiveCardPoints();
+        int expensiveCardPoints2 = generator.generateExpensiveCardPoints();
+        int expensiveCardPoints3 = generator.generateExpensiveCardPoints();
+
+        // then
+        assertEquals(expensiveCardPoints1, 3);
+        assertEquals(expensiveCardPoints2, 4);
+        assertEquals(expensiveCardPoints3, 5);
+    }
+}
