a600f2fcaf687e80cc751d1747933dc233c97c18
Bartek <bartek9393@poczta.onet.pl>
2017-03-04 22:46:56 +0100

    Refactor

--- src/app/game/card/CardCostGenerator.java
+++ src/app/game/card/CardCostGenerator.java
@@ -1,5 +1,6 @@
 package app.game.card;
 
+import app.game.token.Tokens;
 import app.util.Random;
 
 public class CardCostGenerator {
@@ -11,5 +12,26 @@ public class CardCostGenerator {
 
     public CardCostGenerator(Random random) {
         this.random = random;
+    }
+
+    public Tokens getCheap() {
+        return getCost(3, 5);
+    }
+
+    public Tokens getMedium() {
+        return getCost(5, 8);
+    }
+
+    public Tokens getExpensive() {
+        return getCost(7, 14);
+    }
+
+    private Tokens getCost(int min, int max) {
+        int[] array = {0, 0, 0, 0, 0};
+        int cost = random.nextInt(min, max);
+        while (cost-- > 0) {
+            array[random.nextInt(0, 5)]++;
+        }
+        return new Tokens(array[0], array[1], array[2], array[3], array[4]);
     }
 }
