9c214b1b094637095b1d6267e8066e9411184778
Bartek <bartek9393@poczta.onet.pl>
2017-03-04 17:04:50 +0100

    Added Nobility Test

---
+++ test/NobilityTest.java
@@ -0,0 +1,24 @@
+import app.MockRandom;
+import app.game.card.nobility.Nobility;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
+public class NobilityTest {
+    @Test
+    public void shouldDrawRandomPoints() {
+        // given
+        Random random = new MockRandom(1, 0);
+        Nobility nobility = new Nobility(random);
+
+        // when
+        int points1 = nobility.getRandomPoints();
+        int points2 = nobility.getRandomPoints();
+
+        // then
+        assertEquals(points1, 4);
+        assertEquals(points2, 3);
+    }
+}
