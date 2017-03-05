8eec200040ec50d0c3e81cc41ee00417290a478e
Bartek <bartek9393@poczta.onet.pl>
2017-03-05 18:03:41 +0100

    Implemented invoke()

--- src/app/game/turn/ReservationTurn.java
+++ src/app/game/turn/ReservationTurn.java
@@ -1,15 +1,24 @@
 package app.game.turn;
 
 import app.game.Game;
+import app.game.Player;
 import app.game.card.Card;
+import app.game.token.Tokens;
 
 public class ReservationTurn extends Turn {
-    public ReservationTurn(Card card) {
+    private Card card;
 
+    public ReservationTurn(Card card) {
+        this.card = card;
     }
 
     @Override
     public void invoke(Game game) {
-
+        Player player = game.getCurrentPlayer();
+        Tokens playerTokens = player.getTokens();
+        Tokens tokensAfterReservation = new Tokens(playerTokens.getGreen(), playerTokens.getPurple() , playerTokens.getBlue() , playerTokens.getBlack() , playerTokens.getRed(), playerTokens.getVersatile() + 1);
+        player.setTokens(tokensAfterReservation);
+        card.setReserved(true);
+        player.addCard(card);
     }
 }
