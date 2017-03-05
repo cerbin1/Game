178559ae3fd8367887c484923594a2fff3720eda
Bartek <bartek9393@poczta.onet.pl>
2017-03-05 02:58:23 +0100

    Implemented invoke()

--- src/app/game/turn/BuyCardTurn.java
+++ src/app/game/turn/BuyCardTurn.java
@@ -3,14 +3,27 @@ package app.game.turn;
 import app.game.Game;
 import app.game.Player;
 import app.game.card.Card;
+import app.game.token.Tokens;
 
 public class BuyCardTurn extends Turn {
-    public BuyCardTurn(Card card) {
+    private Card card;
 
+    public BuyCardTurn(Card card) {
+        this.card = card;
     }
 
     @Override
     public void invoke(Game game) {
         Player player = game.getCurrentPlayer();
+        Tokens playerTokens = player.getTokens();
+        Tokens cost = card.getCost();
+        Tokens gameTokens = game.getTokens();
+        player.addCard(card);
+        player.setTokens(getPlayerTokensAfterBuyingCard(playerTokens, cost));
+        game.setTokens(new Tokens(gameTokens.getGreen() + cost.getGreen(), gameTokens.getPurple() + cost.getPurple(), gameTokens.getBlue() + cost.getBlue(), gameTokens.getBlack() + cost.getBlack(), gameTokens.getRed() + cost.getRed()));
+    }
+
+    private Tokens getPlayerTokensAfterBuyingCard(Tokens playerTokens, Tokens cost) {
+        return new Tokens(playerTokens.getGreen() - cost.getGreen(), playerTokens.getPurple() - cost.getPurple(), playerTokens.getBlue() - cost.getBlue(), playerTokens.getBlack() - cost.getBlack(), playerTokens.getRed() - cost.getRed());
     }
 }
