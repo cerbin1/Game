0c13de09eb593a8ae8871caaeaeccaca6fd4fc6c
Bartek <bartek9393@poczta.onet.pl>
2017-03-07 22:08:17 +0100

    Refactored shouldCreatePlayerWithDefaultTokens()

--- test/PlayerTest.java
+++ test/PlayerTest.java
@@ -8,29 +8,20 @@ import app.game.token.Tokens;
 import org.junit.Test;
 
 import static app.game.token.TokenColor.Green;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class PlayerTest {
     @Test
-    public void shouldCreatePlayerWithDefaultPoints() {
+    public void shouldCreatePlayerWithDefaultTokens() {
         // given
-        Player player1 = new Player();
-        Player player2 = new Player(new Tokens(1, 1, 1, 1, 1));
-        Player player3 = new Player(new Tokens(0, 4, 0, 4, 0));
-        Player player4 = new Player(new Tokens(5, 5, 5, 5, 5));
+        Player player;
 
         // when
-        int[] tokensValue1 = {player1.getTokens().getGreen(), player1.getTokens().getPurple(), player1.getTokens().getBlue(), player1.getTokens().getBlack(), player1.getTokens().getRed()};
-        int[] tokensValue2 = {player2.getTokens().getGreen(), player2.getTokens().getPurple(), player2.getTokens().getBlue(), player2.getTokens().getBlack(), player2.getTokens().getRed()};
-        int[] tokensValue3 = {player3.getTokens().getGreen(), player3.getTokens().getPurple(), player3.getTokens().getBlue(), player3.getTokens().getBlack(), player3.getTokens().getRed()};
-        int[] tokensValue4 = {player4.getTokens().getGreen(), player4.getTokens().getPurple(), player4.getTokens().getBlue(), player4.getTokens().getBlack(), player4.getTokens().getRed()};
+        player = new Player(new Tokens(0, 1, 2, 3, 4));
 
         // then
-        assertArrayEquals(new int[]{0, 0, 0, 0, 0}, tokensValue1);
-        assertArrayEquals(new int[]{1, 1, 1, 1, 1}, tokensValue2);
-        assertArrayEquals(new int[]{0, 4, 0, 4, 0}, tokensValue3);
-        assertArrayEquals(new int[]{5, 5, 5, 5, 5}, tokensValue4);
+        assertTrue(new Tokens(0, 1, 2, 3, 4).equals(player.getTokens()));
     }
 
     @Test
