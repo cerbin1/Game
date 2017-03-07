d42aa55f87f8d41c781d5ae56e5a5504d1039cd8
Bartek <bartek9393@poczta.onet.pl>
2017-03-07 22:41:00 +0100

    Added TokenColorTest

---
+++ test/TokenColorTest.java
@@ -0,0 +1,28 @@
+import app.game.token.TokenColor;
+import org.junit.Test;
+
+import static app.game.token.TokenColor.*;
+import static org.junit.Assert.assertEquals;
+
+public class TokenColorTest {
+
+    @Test
+    public void shouldGetRandomCardColor() {
+        // given
+        MockProbability probability = new MockProbability(0, 1, 2, 3, 4);
+
+        // when
+        TokenColor cardColor1 = TokenColor.getRandom(probability);
+        TokenColor cardColor2 = TokenColor.getRandom(probability);
+        TokenColor cardColor3 = TokenColor.getRandom(probability);
+        TokenColor cardColor4 = TokenColor.getRandom(probability);
+        TokenColor cardColor5 = TokenColor.getRandom(probability);
+
+        // then
+        assertEquals(Green, cardColor1);
+        assertEquals(Purple, cardColor2);
+        assertEquals(Blue, cardColor3);
+        assertEquals(Black, cardColor4);
+        assertEquals(Red, cardColor5);
+    }
+}
