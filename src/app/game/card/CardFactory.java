eb28b512d78090e83cfb21714f1a5453b497ebc0
Bartek <bartek9393@poczta.onet.pl>
2017-03-07 22:42:00 +0100

    Changed getting random color to TokenColor.getRandom()

--- src/app/game/card/CardFactory.java
+++ src/app/game/card/CardFactory.java
@@ -1,11 +1,11 @@
 package app.game.card;
 
+import app.game.token.TokenColor;
 import app.util.Probability;
 
 public class CardFactory {
     private final CardCostGenerator cost;
     private final CardPointsGenerator points;
-    private final CardColorGenerator color;
 
     public CardFactory() {
         this(new Probability());
@@ -14,18 +14,17 @@ public class CardFactory {
     public CardFactory(Probability probability) {
         cost = new CardCostGenerator(probability);
         points = new CardPointsGenerator(probability);
-        color = new CardColorGenerator(probability);
     }
 
     public Card createCheapCard() {
-        return new CheapCard(cost.getCheap(), points.generateCheapCardPoints(), color.generateColor());
+        return new CheapCard(cost.getCheap(), points.generateCheapCardPoints(), TokenColor.getRandom());
     }
 
     public Card createMediumCard() {
-        return new MediumCard(cost.getMedium(), points.generateMediumCardPoints(), color.generateColor());
+        return new MediumCard(cost.getMedium(), points.generateMediumCardPoints(), TokenColor.getRandom());
     }
 
     public Card createExpensiveCard() {
-        return new ExpensiveCard(cost.getExpensive(), points.generateExpensiveCardPoints(), color.generateColor());
+        return new ExpensiveCard(cost.getExpensive(), points.generateExpensiveCardPoints(), TokenColor.getRandom());
     }
 }
