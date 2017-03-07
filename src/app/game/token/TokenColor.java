a0efec13ae5ad4bf6b1cf271d8d2d426ae24170d
Bartek <bartek9393@poczta.onet.pl>
2017-03-07 22:41:19 +0100

    Added getRandom()

--- src/app/game/token/TokenColor.java
+++ src/app/game/token/TokenColor.java
@@ -1,5 +1,16 @@
 package app.game.token;
 
+import app.util.Probability;
+
 public enum TokenColor {
-    Green, Purple, Blue, Black, Red
+    Green, Purple, Blue, Black, Red;
+
+    public static TokenColor getRandom() {
+        return getRandom(new Probability());
+    }
+
+    public static TokenColor getRandom(Probability probability) {
+        TokenColor[] tokenColors = TokenColor.values();
+        return tokenColors[probability.nextInt(0, tokenColors.length)];
+    }
 }
