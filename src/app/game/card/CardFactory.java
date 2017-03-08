18ec54b4891d416135d91fa0712d214c84ad7366
Bartek <bartek9393@poczta.onet.pl>
2017-03-08 01:47:08 +0100

    Added field probability and getting it as parameter of TokenColor.getRandom()

--- src/app/game/card/CardFactory.java
+++ src/app/game/card/CardFactory.java
@@ -6,6 +6,7 @@ import app.util.Probability;
 public class CardFactory {
     private final CardCost cost;
     private final CardPoints points;
+    private Probability probability;
 
     public CardFactory() {
         this(new Probability());
@@ -14,17 +15,18 @@ public class CardFactory {
     public CardFactory(Probability probability) {
         cost = new CardCost(probability);
         points = new CardPoints(probability);
+        this.probability = probability;
     }
 
     public Card createCheapCard() {
-        return new CheapCard(cost.getCheap(), points.getCheap(), TokenColor.getRandom());
+        return new CheapCard(cost.getCheap(), points.getCheap(), TokenColor.getRandom(probability));
     }
 
     public Card createMediumCard() {
-        return new MediumCard(cost.getMedium(), points.getMedium(), TokenColor.getRandom());
+        return new MediumCard(cost.getMedium(), points.getMedium(), TokenColor.getRandom(probability));
     }
 
     public Card createExpensiveCard() {
-        return new ExpensiveCard(cost.getExpensive(), points.getExpensive(), TokenColor.getRandom());
+        return new ExpensiveCard(cost.getExpensive(), points.getExpensive(), TokenColor.getRandom(probability));
     }
 }
