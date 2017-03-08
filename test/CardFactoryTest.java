1349fe31938a4f301531d15b70b2e9dd479e397f
Bartek <bartek9393@poczta.onet.pl>
2017-03-08 01:49:35 +0100

    Fixed properly testing methods

--- test/CardFactoryTest.java
+++ test/CardFactoryTest.java
@@ -3,50 +3,49 @@ import app.game.card.CardFactory;
 import app.game.token.Tokens;
 import org.junit.Test;
 
-import static app.game.token.TokenColor.Blue;
-import static app.game.token.TokenColor.Green;
+import static app.game.token.TokenColor.*;
 import static org.junit.Assert.assertEquals;
 
 public class CardFactoryTest {
     @Test
     public void shouldCreateCheapCard() {
         // given
-        CardFactory factory = new CardFactory(new MockProbability(0, 0, 1, 0, 0));
+        CardFactory factory = new CardFactory(new MockProbability(3, 0, 0, 0, 1, 3));
 
         // when
         Card cheapCard = factory.createCheapCard();
 
         // then
         assertEquals(new Tokens(3, 0, 0, 0, 0), cheapCard.getCost());
-        assertEquals(0, cheapCard.getPoints());
-        assertEquals(Green, cheapCard.getColor());
+        assertEquals(1, cheapCard.getPoints());
+        assertEquals(Black, cheapCard.getColor());
     }
 
     @Test
     public void shouldCreateMediumCard() {
         // given
-        CardFactory factory = new CardFactory(new MockProbability(0, 0, 1));
+        CardFactory factory = new CardFactory(new MockProbability(5, 0, 1, 1, 2, 4, 3, 1));
 
         // when
         Card mediumCard = factory.createMediumCard();
 
         // then
-        assertEquals(new Tokens(5, 0, 0, 0, 0), mediumCard.getCost());
-        assertEquals(1, mediumCard.getPoints());
-        assertEquals(Green, mediumCard.getColor());
+        assertEquals(new Tokens(1, 2, 1, 0, 1), mediumCard.getCost());
+        assertEquals(3, mediumCard.getPoints());
+        assertEquals(Purple, mediumCard.getColor());
     }
 
     @Test
     public void shouldCreateExpensiveCard() {
         // given
-        CardFactory builder = new CardFactory(new MockProbability(0, 0, 1));
+        CardFactory builder = new CardFactory(new MockProbability(10, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 4, 0));
 
         // when
         Card expensiveCard = builder.createExpensiveCard();
 
         // then
-        assertEquals(new Tokens(7, 0, 0, 0, 0), expensiveCard.getCost());
-        assertEquals(3, expensiveCard.getPoints());
-        assertEquals(Blue, expensiveCard.getColor());
+        assertEquals(new Tokens(2, 2, 2, 2, 2), expensiveCard.getCost());
+        assertEquals(4, expensiveCard.getPoints());
+        assertEquals(Green, expensiveCard.getColor());
     }
 }
