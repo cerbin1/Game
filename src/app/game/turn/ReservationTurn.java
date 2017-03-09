3ca6ca5b93d894a362ad7266a3c3d17ce6b758a2
Bartek <bartek9393@poczta.onet.pl>
2017-03-09 19:01:53 +0100

    Changed previous setting versatile into updateVersatile()

--- src/app/game/turn/ReservationTurn.java
+++ src/app/game/turn/ReservationTurn.java
@@ -3,7 +3,6 @@ package app.game.turn;
 import app.game.Game;
 import app.game.Player;
 import app.game.card.Card;
-import app.game.token.Tokens;
 
 public class ReservationTurn extends Turn {
     private Card card;
@@ -15,9 +14,8 @@ public class ReservationTurn extends Turn {
     @Override
     public void invoke(Game game) {
         Player player = game.getCurrentPlayer();
-        Tokens playerTokens = player.getTokens();
-        Tokens tokensAfterReservation = new Tokens(playerTokens.getGreen(), playerTokens.getPurple(), playerTokens.getBlue(), playerTokens.getBlack(), playerTokens.getRed(), playerTokens.getVersatile() + 1);
-        player.setTokens(tokensAfterReservation);
+        player.updateVersatile(1);
+        game.updateVersatile(-1);
         game.removeCard(card);
         card.setReserved(true);
         player.addCard(card);
