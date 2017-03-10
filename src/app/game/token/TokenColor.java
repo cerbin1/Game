c64e45d315bdb86f6aff49600b19449898d3a40b
Daniel <wilkowski.kontakt@gmail.com>
2017-03-10 18:17:09 +0100

    Removed TokenColor.getRandom()

--- src/app/game/token/TokenColor.java
+++ src/app/game/token/TokenColor.java
@@ -5,10 +5,6 @@ import app.util.Probability;
 public enum TokenColor {
     Green, Purple, Blue, Black, Red;
 
-    public static TokenColor getRandom() {
-        return getRandom(new Probability());
-    }
-
     public static TokenColor getRandom(Probability probability) {
         int index = probability.nextInt(0, values().length);
         return values()[index];
