1cb89708da2c5a3e4f5b82fd8294d866ee2078ed
Bartek <bartek9393@poczta.onet.pl>
2017-03-11 21:35:52 +0100

    Implemented invoke()

--- src/app/game/turn/BuyCardTurn.java
+++ src/app/game/turn/BuyCardTurn.java
@@ -3,6 +3,7 @@ package app.game.turn;
 import app.game.Game;
 import app.game.Player;
 import app.game.card.Card;
+import app.game.token.Resources;
 import app.game.token.Tokens;
 
 public class BuyCardTurn extends Turn {
@@ -14,5 +15,18 @@ public class BuyCardTurn extends Turn {
 
     @Override
     public void invoke(Game game) {
+        Player player = game.getCurrentPlayer();
+        Resources resources = player.getResourcesNEW();
+        Tokens cardCost = card.getCost();
+        if (!resources.canBuy(cardCost) || !game.getAvailableCards().contains(card) && !player.getCards().contains(card)) {
+            throw new IllegalTurnException();
+        } else {
+            Tokens tokensSpent = player.getTokens().subtract(resources.calculateChange(cardCost));
+            game.setTokens(game.getTokens().add(tokensSpent));
+            player.setTokens(resources.calculateChange(cardCost));
+            card.setReserved(false);
+            game.removeCard(card);
+            player.addCard(card);
+        }
     }
 }
