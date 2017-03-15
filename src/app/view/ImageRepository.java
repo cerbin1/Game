5e4a7fedaffc369e6d405c7f5f5cefb677ad8867
Bartek <bartek9393@poczta.onet.pl>
2017-03-15 23:19:07 +0100

    Added nobility

--- src/app/view/ImageRepository.java
+++ src/app/view/ImageRepository.java
@@ -15,6 +15,7 @@ public class ImageRepository {
     public BufferedImage cardBack = get("images/card.png");
     public BufferedImage cardBench = get("images/cards/view/bench.png");
     public BufferedImage cardCastle = get("images/cards/view/castle.png");
+    public BufferedImage nobility = get("images/nobilities/nobility.png");
 
     public BufferedImage background1 = get("images/background_1.png");
 
