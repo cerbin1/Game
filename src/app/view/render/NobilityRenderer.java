8d07c91649aadc0b68334f1d8e52ae4663bc7f07
Bartek <bartek9393@poczta.onet.pl>
2017-03-16 19:31:10 +0100

    Added drawing points and topHeader

--- src/app/view/render/NobilityRenderer.java
+++ src/app/view/render/NobilityRenderer.java
@@ -1,20 +1,58 @@
 package app.view.render;
 
+import app.game.card.nobility.Nobility;
 import app.view.ImageRepository;
 
 import java.awt.*;
+import java.awt.font.GlyphVector;
 import java.awt.image.BufferedImage;
 
+import static java.awt.Color.black;
+import static java.awt.Color.white;
+import static java.awt.Font.ITALIC;
+import static java.awt.Font.PLAIN;
+
 public class NobilityRenderer extends Renderer {
-    private final BufferedImage nobility;
+    private final BufferedImage nobilityImage;
+    private final Nobility nobility;
+    private Font pointsFont = new Font("Franklin Gothic Medium", ITALIC, 70);
+    private Font costFont = new Font("Franklin Gothic Medium", PLAIN, 40);
 
     public NobilityRenderer(NobilityVO nobilityVO) {
         super(nobilityVO);
-        nobility = ImageRepository.imageRepository().nobility;
+        nobility = nobilityVO.getNobility();
+        nobilityImage = ImageRepository.imageRepository().nobility;
     }
 
     @Override
     protected void render(Graphics2D graphics) {
-        graphics.drawImage(nobility, 0, 0, null);
+        graphics.drawImage(nobilityImage, 0, 0, null);
+        drawTopHeader(graphics);
+    }
+
+    private void drawTopHeader(Graphics2D graphics) {
+        graphics.setColor(new Color(255, 255, 255, 180));
+        graphics.fillRoundRect(0, 0, nobilityImage.getWidth(), 80,
+                20, 20);
+
+        graphics.setColor(black);
+        graphics.setFont(pointsFont);
+        if (nobility.getPoints() > 0) {
+            drawOutlineText(graphics, nobility.getPoints() + "", 20, 66);
+        }
+    }
+
+    private void drawOutlineText(Graphics2D graphics, String text, int x, int y) {
+        graphics.translate(x, y);
+        GlyphVector glyphVector = pointsFont.createGlyphVector(graphics.getFontRenderContext(), text);
+        Shape textShape = glyphVector.getOutline();
+
+        graphics.setColor(black);
+        graphics.setStroke(new BasicStroke(2.0f));
+        graphics.draw(textShape);
+
+        graphics.setColor(white);
+        graphics.fill(textShape);
+        graphics.translate(-x, -y);
     }
 }
