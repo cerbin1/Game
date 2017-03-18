708ceac69601033f74ce2ee0af12c62c625cc543
Bartek <bartek9393@poczta.onet.pl>
2017-03-18 17:26:30 +0100

    clean

--- src/app/view/render/ButtonVO.java
+++ src/app/view/render/ButtonVO.java
@@ -1,6 +1,6 @@
 package app.view.render;
 
-public class ButtonVO extends ViewObject{
+public class ButtonVO extends ViewObject {
     public ButtonVO(int x, int y) {
         super(x, y, 670, 126);
     }
