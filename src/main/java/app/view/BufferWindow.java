38986b83dccac617d1a1e39a49be3f2c72ae66f2
Daniel <wilkowski.kontakt@gmail.com>
2017-03-19 16:18:38 +0100

    clean BufferWindow

--- src/main/java/app/view/BufferWindow.java
+++ src/main/java/app/view/BufferWindow.java
@@ -7,17 +7,12 @@ import static java.awt.RenderingHints.*;
 import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 
 public class BufferWindow extends Window {
+    private final BufferedImage backBuffer = new BufferedImage(1920, 1080, TYPE_INT_ARGB);
+    private final Graphics2D canvas = backBuffer.createGraphics();
+
     private Graphics windowGraphics;
-    private BufferedImage backBuffer;
-    private Graphics2D canvas;
 
     public BufferWindow() {
-        initializeBackBuffer();
-    }
-
-    private void initializeBackBuffer() {
-        backBuffer = new BufferedImage(1920, 1080, TYPE_INT_ARGB);
-        canvas = backBuffer.createGraphics();
         canvas.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
         canvas.setRenderingHint(KEY_RENDERING, VALUE_RENDER_QUALITY);
     }
