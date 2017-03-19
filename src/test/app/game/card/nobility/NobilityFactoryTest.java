3211674d85027699ae6d38a3e7788f5cc2f80ef1
Daniel <wilkowski.kontakt@gmail.com>
2017-03-19 15:03:11 +0100

    Cleaned NobilityFactoryTest

--- src/test/app/game/card/nobility/NobilityFactoryTest.java
+++ src/test/app/game/card/nobility/NobilityFactoryTest.java
@@ -12,19 +12,21 @@ public class NobilityFactoryTest {
     public void shouldCreateNobility() {
         // given
         Probability probability = mock(Probability.class);
+        NobilityFactory factory = new NobilityFactory(probability);
+
         when(probability.nextInt(0, 1)).thenReturn(0);
         when(probability.nextInt(0, 4)).thenReturn(1, 2, 3);
         when(probability.nextInt(3, 4)).thenReturn(4);
-        NobilityFactory factory = new NobilityFactory(probability);
 
         // when
         Nobility nobility = factory.create();
 
         // then
-        verify(probability, times(1)).nextInt(0, 1);
-        verify(probability, times(1)).nextInt(3, 4);
-        verify(probability, times(3)).nextInt(0, 4);
         assertEquals(new Tokens(0, 3, 3, 3, 0), nobility.getCost());
         assertEquals(4, nobility.getPoints());
+
+        verify(probability, times(1)).nextInt(0, 1);
+        verify(probability, times(3)).nextInt(0, 4);
+        verify(probability, times(1)).nextInt(3, 4);
     }
 }
