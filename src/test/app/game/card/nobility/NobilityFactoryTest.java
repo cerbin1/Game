deba835618e6433687b5650891e17fbfb99a9462
Bartek <bartek9393@poczta.onet.pl>
2017-03-19 14:37:44 +0100

    Made NobilityFactoryTest test create() only

--- src/test/app/game/card/nobility/NobilityFactoryTest.java
+++ src/test/app/game/card/nobility/NobilityFactoryTest.java
@@ -9,46 +9,11 @@ import static org.mockito.Mockito.*;
 
 public class NobilityFactoryTest {
     @Test
-    public void shouldDrawRandomPoints() {
-        // given
-        Probability probability = mock(Probability.class);
-        when(probability.nextInt(0, 1)).thenReturn(0);
-        when(probability.nextInt(0, 4)).thenReturn(0, 1, 2);
-        when(probability.nextInt(3, 4)).thenReturn(3);
-        NobilityFactory factory = new NobilityFactory(probability);
-
-        // when
-        Nobility nobility = factory.create();
-
-        // then
-        verify(probability, times(1)).nextInt(3, 4);
-        assertEquals(3, nobility.getPoints());
-    }
-
-    @Test
-    public void shouldRandomThreeCostTypes() {
+    public void shouldCreateNobility() {
         // given
         Probability probability = mock(Probability.class);
         when(probability.nextInt(0, 1)).thenReturn(0);
         when(probability.nextInt(0, 4)).thenReturn(1, 2, 3);
-        when(probability.nextInt(3, 4)).thenReturn(3);
-        NobilityFactory factory = new NobilityFactory(probability);
-
-        // when
-        Nobility nobility = factory.create();
-
-        // then
-        verify(probability, times(1)).nextInt(0, 1);
-        verify(probability, times(3)).nextInt(0, 4);
-        assertEquals(new Tokens(0, 3, 3, 3, 0), nobility.getCost());
-    }
-
-    @Test
-    public void shouldRandomTwoCostTypes() {
-        // given
-        Probability probability = mock(Probability.class);
-        when(probability.nextInt(0, 1)).thenReturn(1);
-        when(probability.nextInt(0, 4)).thenReturn(3, 4);
         when(probability.nextInt(3, 4)).thenReturn(4);
         NobilityFactory factory = new NobilityFactory(probability);
 
@@ -57,7 +22,9 @@ public class NobilityFactoryTest {
 
         // then
         verify(probability, times(1)).nextInt(0, 1);
-        verify(probability, times(2)).nextInt(0, 4);
-        assertEquals(new Tokens(0, 0, 0, 4, 4), nobility.getCost());
+        verify(probability, times(1)).nextInt(3, 4);
+        verify(probability, times(3)).nextInt(0, 4);
+        assertEquals(new Tokens(0, 3, 3, 3, 0), nobility.getCost());
+        assertEquals(4, nobility.getPoints());
     }
 }
