edac1956672eed361c1eb1c9be9db64b0a16bf37
cerbin1 <bartek9393@poczta.onet.pl>
2017-03-20 15:04:59 +0100

    Implemented Resolution.scaleFullHdToResolution

--- src/main/java/app/view/render/ViewObject.java
+++ src/main/java/app/view/render/ViewObject.java
@@ -1,6 +1,7 @@
 package app.view.render;
 
 import app.model.Updatable;
+import app.view.Resolution;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -27,6 +28,7 @@ public abstract class ViewObject implements Updatable {
     final public Shape getOutline() {
         AffineTransform transform = new AffineTransform();
         transform.scale(0.6, 0.6);
+        Resolution.scaleFullHdToResolution(transform);
         transform.translate(getX(), getY());
         transform.scale(getPerspectiveX(), getPerspectiveY());
         transform.rotate(getRotation());
