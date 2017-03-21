126992fcb4fe3fd5416e82294e1a81a27e91260e
Bartek <bartek9393@poczta.onet.pl>
2017-03-21 19:58:41 +0100

    Implementing Table

--- src/main/java/app/presenter/Table.java
+++ src/main/java/app/presenter/Table.java
@@ -5,15 +5,12 @@ import app.view.render.vo.CardVO;
 import app.view.render.vo.TokenVO;
 import app.view.render.vo.ViewObject;
 
-import java.util.EnumMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static app.model.token.TokenColor.*;
 
 public class Table {
-    private final Set<ViewObject> viewObjects = new HashSet<>();
+    private final List<ViewObject> viewObjects = new ArrayList<>();
     private final Map<TokenColor, Integer> tokenColors = new EnumMap<>(TokenColor.class);
 
     public Table() {
@@ -25,12 +22,16 @@ public class Table {
     }
 
     boolean put(ViewObject vo) {
-        if (viewObjects.isEmpty() && (vo instanceof CardVO || vo instanceof TokenVO)) {
-            if (vo instanceof TokenVO && !((TokenVO) vo).isVersatile()) {
-                putTokenColor(vo);
+        if (viewObjects.isEmpty()) {
+            if ((vo instanceof CardVO || vo instanceof TokenVO)) {
+                if (vo instanceof TokenVO) {
+                    if (!((TokenVO) vo).isVersatile()) {
+                        putTokenColor(vo);
+                    }
+                }
+                viewObjects.add(vo);
+                return true;
             }
-            viewObjects.add(vo);
-            return true;
         }
         if (vo instanceof CardVO) {
             if (viewObjects.size() == 1) {
@@ -51,25 +52,31 @@ public class Table {
                     return false;
                 }
             }
-            if (viewObjects.size() == 1 && viewObjects.stream().anyMatch(v -> (v instanceof TokenVO) && !((TokenVO) v).isVersatile())) {
-                putTokenColor(vo);
-                viewObjects.add(vo);
-                return true;
+            if (viewObjects.size() == 1) {
+                if (viewObjects.stream().anyMatch(v -> (v instanceof TokenVO) && !((TokenVO) v).isVersatile())) {
+                    putTokenColor(vo);
+                    viewObjects.add(vo);
+                    return true;
+                }
             }
-            if (viewObjects.size() == 2 && tokenColors.values().stream().noneMatch(integer -> integer == 2)) {
-                putTokenColor(vo);
-                viewObjects.add(vo);
-                return true;
+            if (viewObjects.size() == 2) {
+                if (viewObjects.stream().anyMatch(v -> ((TokenVO) v).getColor() == ((TokenVO) vo).getColor())) {
+                    return false;
+                }
+                if (tokenColors.values().stream().noneMatch(integer -> integer == 2)) {
+                    putTokenColor(vo);
+                    viewObjects.add(vo);
+                    return true;
+                }
             }
-            return false;
         }
         return false;
     }
 
     private void putTokenColor(ViewObject vo) {
         TokenVO tokenVO = (TokenVO) vo;
-        tokenColors.put(tokenVO.getColor(), tokenColors.get(tokenVO.getColor()) + 1);
-        System.out.println(tokenColors.get(tokenVO.getColor()));
+        TokenColor color = tokenVO.getColor();
+        tokenColors.put(color, tokenColors.get(color) + 1);
     }
 
     public void take(ViewObject vo) {
