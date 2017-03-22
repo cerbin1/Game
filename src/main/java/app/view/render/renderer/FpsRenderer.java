1b465c07c818ff84ba1e72acdf504eedf9e08fc8
Bartek <bartek9393@poczta.onet.pl>
2017-03-22 22:06:12 +0100

    Added drawOutline()

--- src/main/java/app/view/render/renderer/FpsRenderer.java
+++ src/main/java/app/view/render/renderer/FpsRenderer.java
@@ -4,9 +4,11 @@ import app.model.Updatable;
 import app.view.render.vo.ViewObject;
 
 import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
 import java.text.DecimalFormat;
 
-import static java.awt.Color.*;
+import static java.awt.Color.WHITE;
+import static java.awt.Color.black;
 
 public class FpsRenderer extends Renderer implements Updatable {
     private int frames = 0;
@@ -24,6 +26,7 @@ public class FpsRenderer extends Renderer implements Updatable {
         double fps = frames / seconds;
         graphics.setColor(WHITE);
         graphics.drawString(new DecimalFormat("##.#").format(fps) + "", 0, 0);
+        drawOutline(graphics);
     }
 
     @Override
@@ -33,6 +36,16 @@ public class FpsRenderer extends Renderer implements Updatable {
             seconds = 0;
             frames = 0;
         }
+    }
+
+    private void drawOutline(Graphics2D graphics) {
+        graphics.setStroke(new BasicStroke(3));
+        graphics.setColor(black);
+        graphics.draw(new RoundRectangle2D.Float(
+                -10, -35,
+                100, 50,
+                20, 20
+        ));
     }
 
     @Override
