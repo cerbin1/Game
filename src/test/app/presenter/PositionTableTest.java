bffe6a9745e86943c15990647dabc0c294f2a61b
Bartek <bartek9393@poczta.onet.pl>
2017-03-31 22:27:37 +0100

    Added tests

--- src/test/app/presenter/PositionTableTest.java
+++ src/test/app/presenter/PositionTableTest.java
@@ -4,15 +4,21 @@ import app.model.card.CheapCard;
 import app.model.token.Token;
 import app.view.render.vo.CardVO;
 import app.view.render.vo.TokenVO;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.awt.*;
 
 import static app.model.token.TokenColor.Green;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.rules.ExpectedException.none;
 
 public class PositionTableTest {
+    @Rule
+    public ExpectedException expectedException = none();
+
     private static PositionTable positionTable() {
         return new PositionTable(0, 0);
     }
@@ -59,17 +65,81 @@ public class PositionTableTest {
     }
 
     @Test
-    public void shouldRemoveTableableAfterTake() {
+    public void shouldTakeTableable() {
         // given
         PositionTable table = positionTable();
         CardVO cardVO = cardVO();
         table.put(cardVO);
 
         // when
-        boolean didTake = table.take(cardVO);
+        boolean canTake = table.take(cardVO);
 
         // then
-        assertTrue(didTake);
+        assertTrue(canTake);
+    }
+
+    @Test
+    public void shouldRemoveTableablePositionAfterTake() {
+        // given
+        PositionTable table = positionTable();
+        CardVO cardVO = cardVO();
+        table.put(cardVO);
+
+        // when
+        table.take(cardVO);
+
+        // then
+        assertTrue(table.isEmpty());
+    }
+
+    @Test
+    public void shouldThrowOnUnexpectedTake() {
+        // given
+        PositionTable table = positionTable();
+        CardVO cardVO = cardVO();
+
+        expectedException.expect(UnexpectedTakeException.class);
+
+        // when
+        table.take(cardVO);
+    }
+
+    @Test
+    public void shouldGather() {
+        // given
+        PositionTable table = positionTable();
+        table.put(cardVO());
+
+        // when
+        boolean canGather = table.canGather();
+
+        // then
+        assertTrue(canGather);
+    }
+
+    @Test
+    public void shouldNotGather() {
+        // given
+        PositionTable table = positionTable();
+        table.put(tokenVO());
+
+        // when
+        boolean canGather = table.canGather();
+
+        // then
+        assertFalse(canGather);
+    }
+
+    @Test
+    public void shouldClearPreviousPositionsAfterGather() {
+        // given
+        PositionTable table = positionTable();
+        table.put(cardVO());
+
+        // when
+        table.gather();
+
+        // then
         assertTrue(table.isEmpty());
     }
 }
