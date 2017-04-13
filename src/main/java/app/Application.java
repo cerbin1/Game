572ebd8415a5bcd0c6f09abc912c7e110edf6aa8
Daniel <wilkowski.kontakt@gmail.com>
2017-04-13 13:15:26 +0200

    Added lwjgl

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,19 +1,44 @@
 package app;
 
-import app.model.EngineExecutor;
-import app.model.EngineFactory;
 import app.presenter.GameWindow;
+import org.newdawn.slick.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Application {
-    public static void main(String... args) {
-        GameWindow window = new GameWindow();
-        window.show();
+    public static void main(String[] args) {
+        try {
+            LwjglWindow game = new LwjglWindow("Simple Slick Game");
 
-        EngineFactory factory = new EngineFactory();
-        factory.addRenderListener(window::render);
-        factory.addUpdateListener(window);
+            AppGameContainer container = new AppGameContainer(game);
+            container.setDisplayMode(1920, 1080, true);
+            container.start();
+        } catch (SlickException ex) {
+            Logger.getLogger(LwjglWindow.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-        EngineExecutor executor = factory.createExecutor();
-        executor.start();
+    static class LwjglWindow extends BasicGame {
+        GameWindow window;
+
+        private LwjglWindow(String gameName) {
+            super(gameName);
+        }
+
+        @Override
+        public void init(GameContainer gc) throws SlickException {
+            window = new GameWindow();
+        }
+
+        @Override
+        public void update(GameContainer gc, int delta) throws SlickException {
+            window.update(delta / 1000.0);
+        }
+
+        @Override
+        public void render(GameContainer gc, Graphics graphics) throws SlickException {
+            window.render(graphics);
+        }
     }
 }
