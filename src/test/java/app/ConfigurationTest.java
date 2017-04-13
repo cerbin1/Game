ebfd963cf9bd366c7048f285dbfc5e69347ca2f0
Bartek <bartek9393@poczta.onet.pl>
2017-04-13 18:54:25 +0100

    Added ConfigurationTest

---
+++ src/test/java/app/ConfigurationTest.java
@@ -0,0 +1,22 @@
+package app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConfigurationTest {
+    @Test
+    public void shouldGetConfiguration() {
+        // when
+        boolean debug = Configuration.getDebug();
+        int width = Configuration.getWidth();
+        int height = Configuration.getHeight();
+        boolean fullscreen = Configuration.getFullscreen();
+
+        // then
+        assertEquals(false, debug);
+        assertEquals(1920, width);
+        assertEquals(1080, height);
+        assertEquals(true, fullscreen);
+    }
+}
