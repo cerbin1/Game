4bc68058ba29b07073ac86029a57c334bf004463
Bartek <bartek9393@poczta.onet.pl>
2017-04-14 23:19:18 +0100

    Implemented applyConfigurationFromArgs()

--- src/main/java/app/config/ConfigurationLoader.java
+++ src/main/java/app/config/ConfigurationLoader.java
@@ -4,19 +4,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.Arrays.asList;
 
 public class ConfigurationLoader {
     private final Properties properties;
-
-    private final List<String> propertyKeys = asList("debug", "size", "fullscreen");
+    private final Map<String, List> propertiesMap = initializeMap();
     private final List[] possibleValues = {
-            asList("true", "false"), asList("1920x1080", "1280x720"), asList("true", "false")};
+            asList("true", "false"), asList("1920x1080", "1280x720", "1366x768", "1600x900"), asList("true", "false")};
 
-    public ConfigurationLoader(String[] args) {
+    private Map<String, List> initializeMap() {
+        Map<String, List> map = new HashMap<>();
+        map.put("debug", possibleValues[0]);
+        map.put("size", possibleValues[1]);
+        map.put("fullscreen", possibleValues[2]);
+        return map;
+    }
+
+    public ConfigurationLoader(String... args) {
         properties = getProperties();
         applyConfigurationFromArgs(args);
     }
@@ -51,8 +62,7 @@ public class ConfigurationLoader {
             FileOutputStream fileOutputStream = new FileOutputStream("config.properties");
             Properties properties = new Properties();
             properties.setProperty("debug", "true");
-            properties.setProperty("width", "1920");
-            properties.setProperty("height", "1080");
+            properties.setProperty("size", "1920x1080");
             properties.setProperty("fullscreen", "true");
             properties.store(fileOutputStream, null);
             fileOutputStream.close();
@@ -63,7 +73,17 @@ public class ConfigurationLoader {
     }
 
     private void applyConfigurationFromArgs(String... args) {
-        // TODO implement
+        for (String string : args) {
+            String key, value;
+            Matcher matcher = Pattern.compile("=").matcher(string);
+            if (matcher.find()) {
+                key = string.substring(0, matcher.start());
+                value = string.substring(matcher.end());
+                if (propertiesMap.containsKey(key) && propertiesMap.get(key).contains(value)) {
+                    properties.setProperty(key, value);
+                }
+            }
+        }
     }
 
     public String getProperty(String name) {
