23445e2b2cb034445f588c03c75fddac39d4525a
Bartek <bartek9393@poczta.onet.pl>
2017-04-14 17:52:21 +0100

    Added createNewConfigurationFile() and loadConfigurationFromArguments()

--- src/main/java/app/ConfigurationLoader.java
+++ src/main/java/app/ConfigurationLoader.java
@@ -2,22 +2,38 @@ package app;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigurationLoader {
-    private Properties properties;
-    private Properties defaultProperties;
+    private Properties properties = new Properties();
 
     ConfigurationLoader() {
         setConfiguration();
     }
 
     void setConfiguration() {
-        loadDefaultConfiguration();
         File config = new File("config.properties");
         if (config.exists()) {
             loadExistingConfiguration();
+        } else {
+            createNewConfigurationFile();
+        }
+    }
+
+    private void createNewConfigurationFile() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("config.properties");
+            properties = new Properties();
+            properties.setProperty("debug", "true");
+            properties.setProperty("width", "1920");
+            properties.setProperty("height", "1080");
+            properties.setProperty("fullscreen", "true");
+            properties.store(fileOutputStream, null);
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.getStackTrace();
         }
     }
 
@@ -31,23 +47,11 @@ public class ConfigurationLoader {
         }
     }
 
-    void loadDefaultConfiguration() {
-        try {
-            defaultProperties = new Properties();
-            FileInputStream in = new FileInputStream("config.defaultProperties");
-            defaultProperties.load(in);
-            in.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        properties = new Properties(defaultProperties);
+    public void loadConfigurationFromArguments(String... args) {
+        new CustomizeConfiguration(properties, args);
     }
 
     public Properties getProperties() {
         return properties;
-    }
-
-    public static void acceptStringParams(String[] args) {
-
     }
 }
