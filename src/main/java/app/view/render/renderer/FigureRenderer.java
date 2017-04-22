3164b45785cc11ac7ca625546c31abaa395bbac1
Bartek <bartek9393@poczta.onet.pl>
2017-04-22 23:56:58 +0100

    Extracted fonts to Font class

--- src/main/java/app/view/render/renderer/FigureRenderer.java
+++ src/main/java/app/view/render/renderer/FigureRenderer.java
@@ -3,23 +3,16 @@ package app.view.render.renderer;
 import app.model.card.Figure;
 import app.model.token.TokenColor;
 import app.view.render.vo.FigureVO;
+import app.view.util.Font;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.TrueTypeFont;
 
 import java.util.function.BiConsumer;
 
-import static java.awt.Font.BOLD;
-import static java.awt.Font.PLAIN;
 import static org.newdawn.slick.Color.white;
 
 abstract class FigureRenderer extends Renderer {
-    private final java.awt.Font pointsFont = new java.awt.Font("Franklin Gothic Medium", BOLD, 52);
-    private final java.awt.Font costFont = new java.awt.Font("Franklin Gothic Medium", PLAIN, 40);
-
-    TrueTypeFont ttf1 = new TrueTypeFont(pointsFont, true);
-    TrueTypeFont ttf2 = new TrueTypeFont(costFont, true);
 
     private final Figure figure;
     private final Image figureImage;
@@ -43,7 +36,7 @@ abstract class FigureRenderer extends Renderer {
         graphics.fillRoundRect(0, 0, figureImage.getWidth(), 80, 20, 20);
 
         if (figure.getPoints() > 0) {
-            ttf1.drawString(20, 6, figure.getPoints() + "", Color.black);
+            Font.POINTS.drawString(20, 6, figure.getPoints() + "", Color.black);
         }
     }
 
@@ -62,7 +55,7 @@ abstract class FigureRenderer extends Renderer {
         @Override
         public void accept(TokenColor color, Integer amount) {
             if (amount == 0) return;
-            ttf2.drawString(19, nextElementHeight(), color.name() + ": " + amount);
+            Font.COST.drawString(19, nextElementHeight(), color.name() + ": " + amount);
         }
 
         private int nextElementHeight() {
---
+++ src/main/java/app/view/util/Font.java
@@ -0,0 +1,13 @@
+package app.view.util;
+
+import org.newdawn.slick.TrueTypeFont;
+
+import java.awt.*;
+
+import static java.awt.Font.BOLD;
+import static java.awt.Font.PLAIN;
+
+public class Font {
+    public static TrueTypeFont POINTS = new TrueTypeFont(new java.awt.Font("Franklin Gothic Medium", BOLD, 52), true);
+    public static TrueTypeFont COST = new TrueTypeFont(new java.awt.Font("Franklin Gothic Medium", PLAIN, 40), true);
+}
