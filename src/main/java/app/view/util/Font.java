206d609d915492e87cf701e70cdf38f3195a2fe1
Bartek <bartek9393@poczta.onet.pl>
2017-05-10 21:18:22 +0100

    Changed font to Arial until fix bug

--- src/main/java/app/view/util/Font.java
+++ src/main/java/app/view/util/Font.java
@@ -6,7 +6,7 @@ import static java.awt.Font.BOLD;
 import static java.awt.Font.PLAIN;
 
 public class Font {
-    public TrueTypeFont POINTS_FONT = new TrueTypeFont(new java.awt.Font("Franklin Gothic Medium", BOLD, 52), true);
+    public TrueTypeFont POINTS_FONT = new TrueTypeFont(new java.awt.Font("Arial", BOLD, 52), true);
     public TrueTypeFont COST_FONT = new TrueTypeFont(new java.awt.Font("Franklin Gothic Medium", PLAIN, 40), true);
     public TrueTypeFont BUTTON_FONT = new TrueTypeFont(new java.awt.Font("Arial", PLAIN, 52), true);
 
