ca18680daee4ddc3c714cb86840a1370d8b10ecf
Daniel <wilkowski.kontakt@gmail.com>
2017-05-13 16:17:00 +0200

    Utility

--- src/main/java/app/model/token/TokenColor.java
+++ src/main/java/app/model/token/TokenColor.java
@@ -2,11 +2,18 @@ package app.model.token;
 
 import app.model.util.Probability;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public enum TokenColor {
     Green, Purple, Blue, Black, Red;
 
     public static TokenColor getRandom(Probability probability) {
         int randomColorIndex = probability.nextInt(0, values().length);
         return values()[randomColorIndex];
+    }
+
+    public static Stream<TokenColor> stream() {
+        return Arrays.stream(values());
     }
 }
