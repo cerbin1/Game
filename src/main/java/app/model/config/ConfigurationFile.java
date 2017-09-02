9666737e1b42a2634750f1190979d8dbb8f28284
Daniel <wilkowski.kontakt@gmail.com>
2017-09-02 19:51:53 +0200

    Refactor ConfigurationFile

--- src/main/java/app/model/config/ConfigurationFile.java
+++ src/main/java/app/model/config/ConfigurationFile.java
@@ -13,23 +13,15 @@ class ConfigurationFile {
         return file.exists();
     }
 
-    InputStream getInputStream() {
-        try {
-            return new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            throw new ConfigurationFileException(e);
-        }
+    InputStream getInputStream() throws FileNotFoundException {
+        return new FileInputStream(file);
     }
 
-    OutputStream getOutputStream() {
-        try {
-            boolean alreadyExists = file.createNewFile();
-            if (!alreadyExists) {
-                throw new RuntimeException("File already exists");
-            }
-            return new FileOutputStream(file);
-        } catch (IOException e) {
-            throw new ConfigurationFileException(e);
+    OutputStream getOutputStream() throws IOException {
+        boolean wasMissing = file.createNewFile();
+        if (!wasMissing) {
+            throw new RuntimeException("File existed won't be overwritten");
         }
+        return new FileOutputStream(file);
     }
 }
