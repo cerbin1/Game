7a8e8f7afed775d79fb2602c281e992e7318f61a
Daniel <wilkowski.kontakt@gmail.com>
2017-09-02 19:33:26 +0200

    Removed useless test

--- src/test/java/app/model/turn/TurnFactoryTest.java
+++ src/test/java/app/model/turn/TurnFactoryTest.java
@@ -7,7 +7,6 @@ import app.presenter.UnexpectedGatherException;
 import app.presenter.table.Table;
 import app.view.render.vo.CardVO;
 import app.view.render.vo.TokenVO;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -94,21 +93,6 @@ public class TurnFactoryTest {
         table.put(tokenVO(Blue));
 
         expectedException.expect(UnexpectedGatherException.class);
-
-        // when
-        table.gather();
-    }
-
-    @Test
-    @Ignore
-    public void shouldNotCreateAcquireTokensTurnWithThreeSameTokens() {
-        // given
-        Table table = new Table();
-        table.put(tokenVO(Green));
-        table.put(tokenVO(Green));
-        table.put(tokenVO(Green));  // This is never succeeded
-
-        expectedException.expect(UnexpectedGatherException.class); // so this exception is never thrown
 
         // when
         table.gather();
